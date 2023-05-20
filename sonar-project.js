e84bf1468e678b501796489f9f729e257d4b7d04
EdwinBlewusi <127842278+EdwinBlewusi@users.noreply.github.com>
2023-05-20 12:37:48 -0400

    Updated with sonar url and token

--- sonar-project.js
+++ sonar-project.js
@@ -1,12 +1,12 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://23.22.244.182:8000/',
+  serverUrl: 'http://172.31.84.18:8000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
 	    'sonar.projectKey':'NodeJsJomacsit',
-	    'sonar.login': '73e279fcb46164fb832e53de6d23af5bc2f783f9',
+	    'sonar.login': 'c73d374ef2c3cfbdc0cbd23eb2fe02682a52840b',
 	    //'sonar.password': 'admin',
             'sonar.projectVersion':'1.0',
 	    'sonar.language':'js',
